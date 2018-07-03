3941ed1dd515e40ca6374f451c4d80b1090bf5aa
Manuel Laufenberg <manuel.laufenberg@googlemail.com>
2018-07-03 11:38:53 +0200

    spwan traefik and demo services

--- infrastructure/loadbalancing/access.tf
+++
@@ -1,6 +0,0 @@
-resource "kubernetes_service_account" "traefik" {
-  metadata {
-    namespace = "${kubernetes_namespace.loadbalancing.metadata.0.name}"
-    name      = "traefik-ingress-controller"
-  }
-}
--- infrastructure/loadbalancing/endpoint.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_address" "default" {
-  name    = "tf-gke-k8s-lb"
-  region  = "${var.region}"
-  project = "${var.project_id}"
-}
